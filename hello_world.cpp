6b2db3289257362d3ca26dc537741ac03c26160d
Goydarik <rosgorzion@gmail.com>
2026-04-28 19:56:03 +0300

    Fix comments: translate them into english

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 int main(){
-	string n;//Объявляем n
-	std::cout << "Enter your name: "//Запрашиваем имя
-	std::cin >> n; //Вводим n
-	std::cout << "hello world from " << n;//Выводим текст
+	string n;//Obyavlaem n
+	std::cout << "Enter your name: "//Zaprashivaem ima
+	std::cin >> n; //Vvodim n
+	std::cout << "hello world from " << n;//Vivodim text
 
 
 }
